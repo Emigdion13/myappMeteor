972d3e5fd377c24a54b00d592dfbba5f36f9d906
Emigdion13 <jemigdion@gmail.com>
2022-05-26 16:40:51 -0500

    More Updates!

--- imports/ui/testing.vue
+++ imports/ui/testing.vue
@@ -4,12 +4,14 @@
         <v-btn color="primary" @click="openAlert()">Abrir</v-btn>
         <v-btn color="accent" @click="openLoader()">Loader</v-btn>
         <v-btn color="accent" @click="unload()">Unload</v-btn>
+        <v-btn color="secondary" @click="openModalRemove()">modal Remove</v-btn>
 
         <v-btn class="ma-2" text icon color="red lighten-2">
             <v-icon>mdi-thumb-down</v-icon>
         </v-btn>
         <alert-message ref="refAlertMessageTest"></alert-message>
         <loader ref="refLoader"></loader>
+        <modal-remove ref="refModalRemove" v-bind:modalData="userTemp"></modal-remove>
     </div>
 
 </template>
@@ -17,10 +19,22 @@
 <script>
 import AlertMessage from './components/utilities/alerts/AlertMessage.vue';
 import Loader from './components/utilities/loaders/Loader.vue';
+import ModalRemove from './components/utilities/Modals/ModalRemove.vue';
 
 export default {
-    components: { AlertMessage, Loader },
+    components: { AlertMessage, Loader, ModalRemove },
     name: "Testing",
+    data() {
+        return {
+            userTemp: {
+                preposition: "al",
+                typeElement: "usuario",
+                mainNameElement: '',
+                _id: null,
+                element: {}
+            }
+        }
+    },
     methods: {
         openAlert() {
             this.$refs.refAlertMessageTest.showAlertSimple("success", "Mi tituloasdsadsadsadasdadasdasdsadasdasd", "Cerrar");
@@ -30,6 +44,20 @@ export default {
         },
         unload() {
             this.$refs.refLoader.deactivate();
+        },
+        openModalRemove() {
+            const user = {
+                _id: "151-151-151151515151",
+                name: "Ivan",
+                lastName: "Carera",
+                age: 24,
+                country: "Dominicana"
+            };
+            this.userTemp.element = user;
+            this.userTemp._id = user._id;
+            this.userTemp.mainNameElement = user.name;
+
+            this.$refs.refModalRemove.dialog = true;
         }
     }
 }
