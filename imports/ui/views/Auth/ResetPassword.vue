cb55d86f0edecb94cedf4e0fc5db4f4c92dc7cce
Emigdion13 <jemigdion@gmail.com>
2022-05-28 06:34:41 -0500

    New change

--- imports/ui/views/Auth/ResetPassword.vue
+++ imports/ui/views/Auth/ResetPassword.vue
@@ -1,16 +1,43 @@
 <template>
-  <p>esto es para resetel</p>
+  <div>
+    <div class="title">Reset Password</div>
+    <v-form @submit.prevent="resetPassword">
+      <v-text-field
+        v-model="user.password"
+        id="inputPassword"
+        name="password"
+        autocomplete="new-password"
+        :append-icon="showPass.new?'text':'password'"
+        :type="showPass.new ? 'text':'password'"
+        @click:append="showPass.new = !showPass.new"
+      >
+      </v-text-field>
+    </v-form>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "ResetPassword",
-    mounted() {
-        console.log("token: ", this.$route.params.token);
-    }
-}
+  name: "ResetPassword",
+  data() {
+    return {
+      user: {
+        password: null,
+        confirmPassword: null,
+      },
+      showPass: {
+        new: false,
+        confirm: false,
+      },
+    };
+  },
+  methods: {
+    resetPassword() {
+      console.log("asd");
+    },
+  },
+};
 </script>
 
 <style>
-
 </style>
