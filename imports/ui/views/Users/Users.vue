9cfe95b768269be5bf4cae4e37ea5e72269bb0c7
Emigdion13 <jemigdion@gmail.com>
2022-05-29 17:41:23 -0500

    Changing some INFO

---
+++ imports/ui/views/Users/Users.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-container>
+      <v-row justify="center">
+          <v-col xs="12" sm="12" md="10" lg="8" xl="5">
+              <v-data-table></v-data-table>
+          </v-col>
+      </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+    name:"Users",
+    data(){
+        return {
+            
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
