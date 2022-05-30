3bff17bde62510d8d89fc33202458796189706e3
Emigdion13 <jemigdion@gmail.com>
2022-05-29 23:24:52 -0500

    Adding Filtering to User Tables

--- imports/ui/plugins/vuetify.js
+++ imports/ui/plugins/vuetify.js
@@ -1,36 +1,37 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify';
-import 'vuetify/dist/vuetify.min.css';
-import VueMeteorTracker from 'vue-meteor-tracker';
-import es from 'vuetify/es5/locale/es'; //Traer el idioma en espaniol
-import colors from 'vuetify/lib/util/colors'
+import Vue from "vue";
+import Vuetify from "vuetify";
+import "vuetify/dist/vuetify.min.css";
+import VueMeteorTracker from "vue-meteor-tracker";
+import es from "vuetify/es5/locale/es"; //Traer el idioma en espaniol
+import colors from "vuetify/lib/util/colors";
 
 Vue.use(Vuetify);
 Vue.use(VueMeteorTracker);
 
 export default new Vuetify({
-    theme: {
-        options:{
-            customProperties: true
-        },
-        themes:{
-            light:{
-                primary: colors.blue, // #E53935
-                secondary: colors.blue.lighten1, // #FFCDD2
-                accent: colors.indigo.base, // #3F51B5
-                error: '#d64143',
-                info: '#5bc0de',
-                success: '#5cb85c',
-                warning: '#f0ad4e',
-            }
-        }, icons:{
-            iconfont: 'md'
-        },
-        lang:{
-            locales:{
-                //es
-            },
-            //current: 'es'
-        }
-    }
+  theme: {
+    options: {
+      customProperties: true,
+    },
+    themes: {
+      light: {
+        primary: colors.blue, // #E53935
+        secondary: colors.blue.lighten1, // #FFCDD2
+        accent: colors.indigo.base, // #3F51B5
+        error: "#d64143",
+        info: "#5bc0de",
+        success: "#5cb85c",
+        warning: "#f0ad4e",
+      },
+    },
+  },
+  icons: {
+    //iconfont: "md",
+  },
+  lang: {
+    locales: {
+      //es
+    },
+    //current: 'es'
+  },
 });
--- imports/ui/views/Users/Users.vue
+++ imports/ui/views/Users/Users.vue
@@ -1,33 +1,85 @@
 <template>
   <v-container>
-      <v-row justify="center">
-          <v-col xs="12" sm="12" md="10" lg="8" xl="5">
-              <v-data-table :headers="headers" :items="users" sort-by="name" class="elevation-1"></v-data-table>
-          </v-col>
-      </v-row>
+    <v-row justify="center">
+      <v-col xs="12" sm="12" md="10" lg="8" xl="5">
+        <v-data-table
+          :headers="headers"
+          :items="users"
+          sort-by="name"
+          class="elevation-1"
+        >
+        <template v-slot:body.append>
+            <tr>
+                <td>
+                    <v-text-field v-model="headersFilter.name" type="text" label="Name"></v-text-field>
+                </td>
+                <td>
+                    <v-text-field v-model="headersFilter.username" type="text" label="UserName"></v-text-field>
+                </td>
+                <td>
+                    <v-text-field v-model="headersFilter.email" type="email" label="Email"></v-text-field>
+                </td>
+            </tr>
+
+        </template>
+        
+        </v-data-table>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script>
 export default {
-    name:"Users",
-    data(){
-        return {
-            headers:[
-                {value:'name', text:'Name', sortable:true},
-                {value:'username', text:'UserName', sortable:true},
-                {value:'email', text:'Email', sortable:true}
-            ],
-            users:[
-                {name:'Peter Parker', username: 'spider-Man', email: 'spidy@correo.com'},
-                {name:'Tony Stark', username: 'Iron Man', email: 'iron@correo.com'},
-                {name:'Steve Rogers', username: 'Captain Americana', email: 'captain@correo.com'}
-            ]
-        }
-    }
-}
+  name: "Users",
+  data() {
+    return {
+      headersFilter: {
+        name: "",
+        username: "",
+        email: ""
+      },
+      users: [
+        {
+          name: "Peter Parker",
+          username: "spider-Man",
+          email: "spidy@correo.com"
+        },
+        { name: "Tony Stark", username: "Iron Man", email: "iron@correo.com" },
+        {
+          name: "Steve Rogers",
+          username: "Captain Americana",
+          email: "captain@correo.com"
+        },
+      ],
+    };
+  },
+  computed: {
+    headers() {
+      return [
+        {
+          value: "name",
+          text: "Name",
+          sortable: true,
+          filter: (value) => { return value != null && typeof value === "string" && value.toString().toLowerCase().indexOf(this.headersFilter.name.toLocaleLowerCase()) !== -1 }
+        },
+        {
+          value: "username",
+          text: "UserName",
+          sortable: true,
+          filter: (value) => { return value != null && typeof value === "string" && value.toString().toLowerCase().indexOf(this.headersFilter.name.toLocaleLowerCase()) !== -1 }
+        },
+        {
+          value: "email",
+          text: "Email",
+          sortable: true,
+          filter: (value) => { return value != null && typeof value === "string" && value.toString().toLowerCase().indexOf(this.headersFilter.name.toLocaleLowerCase()) !== -1 }
+        },
+      ];
+    },
+  },
+};
 </script>
 
 <style scoped>
-
 </style>
